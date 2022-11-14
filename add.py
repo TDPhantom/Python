2d8d2df4c7cf8d4e1952473ed7200efcc01d0374
Ansumana BS <Abattlessmith@Brooklynsteamcenter.org>
2022-11-14 10:42:11 -0500

    added

---
+++ add.py
@@ -0,0 +1,4 @@
+a = input("Put a Number")
+b = input("Put a Number")
+
+print(int(a) + int(b))
---
+++ grades.py
@@ -0,0 +1,14 @@
+
+grade = int(input("what is your grade:")) 
+#if you going for adding stuff or using if and elif use int
+
+if grade > 59 and grade < 70: 
+    print("You have an D")
+elif grade > 71 and grade < 79:
+ print("You have an C")
+elif grade > 80 and grade < 89:
+    print("You have an B")
+elif grade > 90 and grade < 100:
+    print("You have an A")
+else:
+    print("you got an F yea loser")
