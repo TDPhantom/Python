af8d53e6455c61f21a1a28adb635c515f2385196
Ansumana BS <Abattlessmith@Brooklynsteamcenter.org>
2022-11-16 10:16:31 -0500

    new

---
+++ functions.py
@@ -0,0 +1,32 @@
+def hello_world():
+    print('Hello world')
+
+    hello_world()
+
+def add(a, b):
+     print(a + b)
+
+
+add(9,10)
+
+a = 15
+b = 10
+
+result = a - b
+
+print(result)
+
+x = 2
+z = 3
+product = x * z
+print(product)
+
+c, f = 7, 3
+result = c//f
+print(result)
+
+k = 3
+l = 2
+Reminder = k % l
+print(Reminder)
+
---
+++ slope.py
@@ -0,0 +1,6 @@
+def slope(m,x,b):
+    return m * x + b
+
+
+y = slope(5,8,3)
+print(y)
