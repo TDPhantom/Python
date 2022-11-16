764924b2d634ad52844d76dd21f9f0f6c70a55ff
Ansumana BS <Abattlessmith@Brooklynsteamcenter.org>
2022-11-16 10:54:17 -0500

    add

---
+++ average.py
@@ -0,0 +1,21 @@
+# x = [90,80,65,0]
+
+# sumcore = 0
+
+# for grade in x:
+#     sumcore = sumcore + grade
+
+# print(sumcore / len(x))
+
+
+
+x = [90,60,80,0]
+
+sumup = 0
+
+for grade in x:
+    sumup = sumup + grade
+
+print(sumup / len(x))
+
+
