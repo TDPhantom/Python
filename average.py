e38e04f4cd727cef33e7922feabca386b041f060
Ansumana BS <Abattlessmith@Brooklynsteamcenter.org>
2022-11-17 10:55:29 -0500

    added

--- average.py
+++ average.py
@@ -8,14 +8,24 @@
 # print(sumcore / len(x))
 
 
+a = int(input("what is your grade:")) 
+y = int(input("what is your grade:")) 
+z = int(input("what is your grade:")) 
+g = int(input("what is your grade:")) 
+v = int(input("what is your grade:")) 
+x = a + y + z + g + v 
+# h = x
+# sumup = 0
 
-x = [90,60,80,0]
+# for grade in x:
+#     sumup = sumup + grade
 
-sumup = 0
-
-for grade in x:
-    sumup = sumup + grade
-
-print(sumup / len(x))
+print("your average grade is")
+print(x / 5)
 
 
+# sales = [25.99, 67.99, 88.99, 155.65, 22.69, 15.99]
+# total = 0
+# for sale in sales:
+#  total = total + sales
+# print("Your total sales were" + str(total))
