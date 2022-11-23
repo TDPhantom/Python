46f88b03b313d088fa530d041b7934e796beb51c
Ansumana BS <Abattlessmith@Brooklynsteamcenter.org>
2022-11-23 09:52:52 -0500

    new

--- turtles.py
+++ turtles.py
@@ -1,11 +1,30 @@
 from turtle import *
-while True is True:
- pendown()
- forward(50)
- penup()
- forward(50)
+# while True is True:
+#  pendown()
+#  forward(50)
+#  penup()
+#  forward(50)
 
-
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
+circle(25)
+forward(50)
 
 
 
